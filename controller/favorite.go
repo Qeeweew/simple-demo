e39b9dd01fb75a59c534dcc5bfef23a0d21d11ac
xix <weijun_xia@icloud.com>
2023-07-29 19:20:52 +0800

    modify FavoriteAction

--- controller/favorite.go
+++ controller/favorite.go
@@ -2,20 +2,12 @@ package controller
 
 import (
 	"net/http"
-	"simple-demo/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
-	token := c.Query("token")
-
-	if _, err := utils.ParseToken(token); err != nil {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
-	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
-	}
 }
 
 // FavoriteList all users have same favorite video list
