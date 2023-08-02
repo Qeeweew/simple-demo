5d3acda594a0401e14c79de51ab6b7858bdab45e
xix <weijun_xia@icloud.com>
2023-08-02 22:31:17 +0800

    refactor: clean up code

--- repository/favorite.go
+++ repository/favorite.go
@@ -17,22 +17,22 @@ func NewFavoriteRepository(db *gorm.DB) model.FavoriteRepository {
 }
 
 func (f *favoriteRepository) UserFavoriteCount(userId uint) (res int64, err error) {
-	err = f.Model(&model.Favorite{UserId: userId}).Count(&res).Error
+	err = f.Where(&model.Favorite{UserId: userId}).Count(&res).Error
 	return
 }
 func (f *favoriteRepository) VideoFavoriteCount(video_id uint) (res int64, err error) {
-	err = f.Model(&model.Favorite{VideoId: video_id}).Count(&res).Error
+	err = f.Where(&model.Favorite{VideoId: video_id}).Count(&res).Error
 	return
 }
 
 func (f *favoriteRepository) UserFavoriteList(userId uint) (res []model.Video, err error) {
-	err = f.Model(&model.Favorite{UserId: userId}).Preload("Video").Preload("Video.Author").Find(&res).Error
+	err = f.Where(&model.Favorite{UserId: userId}).Preload("Video").Preload("Video.Author").Find(&res).Error
 	return
 }
 
 func (f *favoriteRepository) IsFavorite(userId uint, videoId uint) (res bool, err error) {
 	var cnt int64
-	err = f.Model(&model.Favorite{UserId: userId, VideoId: videoId}).Count(&cnt).Error
+	err = f.Where(&model.Favorite{UserId: userId, VideoId: videoId}).Count(&cnt).Error
 	if err != nil {
 		return
 	}
--- repository/relation.go
+++ repository/relation.go
@@ -27,53 +27,53 @@ func (r *relationRepository) FollowList(userId uint) ([]*model.User, error) {
 }
 
 func (r *relationRepository) UnFollow(userId uint, toUserId uint) error {
-	// 检查 user_id 和 to_user_id 是否都存在于数据库中
-	var user, toUser model.User
-	if err := r.DB.First(&user, userId).Error; err != nil {
-		return err
-	}
-	if err := r.DB.First(&toUser, toUserId).Error; err != nil {
-		return err
-	}
-
-	// 使用 Association 方法取消关注关系
-	if err := r.DB.Model(&user).Association("Follows").Delete(&toUser); err != nil {
-		return err
-	}
-
-	return nil
+	return r.DB.Transaction(func(tx *gorm.DB) error {
+		// 检查 user_id 和 to_user_id 是否都存在于数据库中
+		var user, toUser model.User
+		if err := r.DB.First(&user, userId).Error; err != nil {
+			return err
+		}
+		if err := r.DB.First(&toUser, toUserId).Error; err != nil {
+			return err
+		}
+		// 使用 Association 方法取消关注关系
+		if err := r.DB.Model(&user).Association("Follows").Delete(&toUser); err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
 func (r *relationRepository) Follow(userId uint, toUserId uint) error {
-	// 检查 user_id 和 to_user_id 是否都存在于数据库中
-	var user, toUser model.User
-	if err := r.DB.First(&user, userId).Error; err != nil {
-		return err
-	}
-	if err := r.DB.First(&toUser, toUserId).Error; err != nil {
-		return err
-	}
-
-	// 使用 Association 方法创建关注关系
-	if err := r.DB.Model(&user).Association("Follows").Append(&toUser); err != nil {
-		return err
-	}
-
-	return nil
+	return r.DB.Transaction(func(tx *gorm.DB) error {
+		// 检查 user_id 和 to_user_id 是否都存在于数据库中
+		var user, toUser model.User
+		if err := tx.First(&user, userId).Error; err != nil {
+			return err
+		}
+		if err := tx.First(&toUser, toUserId).Error; err != nil {
+			return err
+		}
+		// 使用 Association 方法创建关注关系
+		if err := tx.Model(&user).Association("Follows").Append(&toUser); err != nil {
+			return err
+		}
+		return nil
+	})
 }
 
-func (r *relationRepository) CheckFollowRelationship(userId uint, toUserId uint) (bool, error) {
+func (r *relationRepository) CheckFollowRelationship(userId uint, toUserId uint) (res bool, err error) {
 	var user model.User
-	if err := r.DB.Preload("Follows").First(&user, userId).Error; err != nil {
-		return false, err
+	if err = r.DB.First(&user).Error; err != nil {
+		return
 	}
-
-	for _, follow := range user.Follows {
-		if follow.Id == toUserId {
-			return true, nil
-		}
+	var cnt int64
+	err = r.DB.Table("follow").Where("user_id = ? AND follow_id = ?", userId, toUserId).Count(&cnt).Error
+	if err != nil {
+		return
 	}
-	return false, nil
+	res = cnt > 0
+	return
 }
 
 func NewRelationRepository(db *gorm.DB) model.RelationRepository {
