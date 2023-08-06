7d8691f5b960f4fa10ebefdce1bb8543124d1a48
xix <weijun_xia@icloud.com>
2023-08-06 20:16:25 +0800

    add redis init

--- repository/dbinit/redis.go
+++ repository/dbinit/redis.go
@@ -1,13 +1,22 @@
 package dbinit
 
-// func RedisInit() {
-// 	Redis := redis.NewClient(&redis.Options{
-// 		Addr:     config.RedisCfg.Host,
-// 		Password: config.RedisCfg.Password,
-// 		DB:       0,
-// 	})
-// 	if _, err := Redis.Ping(context.Background()).Result(); err != nil {
-// 		logrus.Panic("connect redis failed: %v", err)
-// 	}
-// 	logrus.Info("Connect redis succeeded")
-// }
+import (
+	"context"
+	"simple-demo/common/config"
+
+	"github.com/redis/go-redis/v9"
+	"github.com/sirupsen/logrus"
+)
+
+func RedisInit() *redis.Client {
+	Redis := redis.NewClient(&redis.Options{
+		Addr:     config.RedisCfg.Host,
+		Password: config.RedisCfg.Password,
+		DB:       0,
+	})
+	if _, err := Redis.Ping(context.Background()).Result(); err != nil {
+		logrus.Panicf("connect redis failed: %v", err)
+	}
+	logrus.Info("Connect redis succeeded")
+	return Redis
+}
